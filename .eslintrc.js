72e2956b6e11e115e2acb6180b2f07437a1186fd
simon3000 <simon30002021@icloud.com>
2020-04-05 00:23:59 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    "node": true,
+    "es2020": true
   },
   extends: [
     'plugin:vue/essential',
@@ -11,6 +12,7 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
+    "space-before-function-paren": "off",
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
